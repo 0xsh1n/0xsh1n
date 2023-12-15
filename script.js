abf4efa8f780af725ec2ea0f75a8def3519e1820
saint <129291449+kinxyz@users.noreply.github.com>
2023-12-15 22:01:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 
+
+function typeWrite(elemento) {
+  const textoArray = elemento.innerHTML.split("");
+  elemento.innerHTML = " ";
+  textoArray.forEach(function (letra, i) {
+    setTimeout(function () {
+      elemento.innerHTML += letra;
+    }, 40 * i);
+  });
+}
+
+function initScrollReveal() {
+  const sr = ScrollReveal({
+    origin: "top",
+    distance: "50px",
+    duration: 2000,
+  });
+
+  sr.reveal(".prj");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  initScrollReveal();
+
+  const typingElement = document.querySelector('.typing-animation');
+  typeWrite(typingElement);
+});
+
